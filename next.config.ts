dc8b482fe191f0ac637a295e0f84b19a71c012ae
Adrian Lopez <adrian.lpes@gmail.com>
2026-02-14 08:36:28 +0200

    added basic setup for nextra

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import nextra from 'nextra';
+
+const withNextra = nextra({});
+
+export default withNextra({
+  turbopack: {
+    resolveAlias: {
+      'next-mdx-import-source-file': './mdx-components.ts',
+    }
+  }
+});
